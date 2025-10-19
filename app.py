0c85f1849370a21e7a81da303b438440073f2a80
SamuelM997 <msam99729@gmail.com>
2025-10-19 11:41:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Store feedbacks in memory (for now)
+feedback_list = []
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_feedback():
+    name = request.form['name']
+    subject = request.form['subject']
+    rating = request.form['rating']
+    comments = request.form['comments']
+
+    feedback = {
+        'name': name,
+        'subject': subject,
+        'rating': rating,
+        'comments': comments
+    }
+
+    feedback_list.append(feedback)
+    return redirect(url_for('view_feedbacks'))
+
+@app.route('/feedbacks')
+def view_feedbacks():
+    return render_template('feedbacks.html', feedbacks=feedback_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
